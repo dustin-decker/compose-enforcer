c73ca024d500b801552fd34c41807076427200b6
Dustin Decker <sevoma@gmail.com>
2017-05-02 21:43:02 -0500

    enabled cli

--- main.go
+++ main.go
@@ -7,32 +7,48 @@ import (
 
 	"github.com/docker/docker/cli/compose/loader"
 	"github.com/docker/docker/cli/compose/types"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	var config *types.Config
-	config, err := LoadConfigFile("test.yml")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	if len(os.Args) == 3 {
+		var config *types.Config
+		config, err := LoadConfigFile(os.Args[2])
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 
-	validations := Validations{
-		StoragePath:       "/dockerstorage",
-		QualifiedService:  "some/service",
-		DockerWrite:       []string{"SwarmCommand"},
-		Secrets:           []string{"secret1", "secret2"},
-		Networks:          []string{"lol"},
-		MemoryLimit:       "3G",
-		CPULimit:          "4",
-		MemoryReservation: "3G",
-		CPUReservation:    "4",
-	}
+		// Get config
+		viper.SetConfigName(os.Args[1]) // name of config file (without extension)
+		viper.AddConfigPath("/")
+		viper.AddConfigPath("$HOME")
+		viper.AddConfigPath(".")
+		err = viper.ReadInConfig() // yaml, toml, json, ini, whatever
+		if err != nil {
+			log.Fatal("Error loading the config file")
+		}
 
-	err = ValidateConfig(validations, config)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+		validations := Validations{
+			StoragePath:       viper.GetString("StoragePath"),
+			QualifiedService:  viper.GetString("QualifiedService"),
+			DockerWrite:       viper.GetStringSlice("DockerWrite"),
+			Secrets:           viper.GetStringSlice("Secrets"),
+			Networks:          viper.GetStringSlice("Networks"),
+			MemoryLimit:       viper.GetString("MemoryLimit"),
+			CPULimit:          viper.GetString("CPULimit"),
+			MemoryReservation: viper.GetString("MemoryReservation"),
+			CPUReservation:    viper.GetString("CPUReservation"),
+		}
 
+		err = ValidateConfig(validations, config)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		log.Print("Success!")
+	} else {
+		log.Fatal("ComposeEnforcer <validation.yml> <docker-compose.yml>")
+	}
 }
 
 // Jacked pretty much the rest of this file from docker/cli/loader_test.go
